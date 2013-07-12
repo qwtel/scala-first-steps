0eae6f0a4555dc6f7931c9aee2a6e8544b65c280
Florian Klampfer <f.klampfer@gmail.com>
2013-07-12 17:22:58 +0200

    improve using currying

--- controlstructures.scala
+++ controlstructures.scala
@@ -5,7 +5,7 @@ def twice(op: Double => Double, x: Double) = op(op(x))
 twice(_ + 1, 5)
 
 // open a resource, operate on it, then close the resource
-def withPrintWriter(file: File, op: PrintWriter => Unit) = {
+def withPrintWriter(file: File)(op: PrintWriter => Unit) = {
   val writer = new PrintWriter(file)
   try {
     op(writer)
@@ -14,9 +14,23 @@ def withPrintWriter(file: File, op: PrintWriter => Unit) = {
   }
 }
 
-// aka "loan pattern"
-withPrintWriter(
-  new File("date.txt"),
-  writer => writer.println(new java.util.Date)
-)
+val file = new File("data.txt")
 
+// using currying, looks like a "native" control structure!
+withPrintWriter(file) {
+  writer => writer.println(new java.util.Date)
+}
+
+// How it works:
+println("Hello world!")
+
+// Can use curly braces for single argument
+println { "Hello world!" }
+
+// but only for single arguments:
+val g = "Hello, World!"
+// g.substring { 7, 9 } // <- error
+g.substring(7, 9) // works
+
+// Therefore: using currying so the last argument is in its own argument list
+// The first argument in parenthesis, the second in curly braces
