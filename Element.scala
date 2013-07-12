b6d49ff682a09625af706f48cff02d47886e9a04
Florian Klampfer <f.klampfer@gmail.com>
2013-07-12 22:21:43 +0200

    make LineElement subclass of Element

--- Element.scala
+++ Element.scala
@@ -2,13 +2,14 @@ abstract class Element {
   def contents: Array[String]
   def height = contents.length
   def width = if (height == 0) 0 else contents(0).length
-  override def toString = contents.mkString("\n")
+  final override def toString = contents.mkString("\n")
 }
 
 class ArrayElement(val contents: Array[String]) extends Element
 
-class LineElement(s: String) extends ArrayElement(Array(s)) {
-  override def width = s.length
+class LineElement(s: String) extends Element {
+  val contents = Array(s)
+  override val width = s.length
   override val height = 1
 }
 
