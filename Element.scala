b8504f5f6e763616dd5134ac6f1e99c1e6a35335
Florian Klampfer <f.klampfer@gmail.com>
2013-07-12 22:30:23 +0200

    beside (idiomatic)

--- Element.scala
+++ Element.scala
@@ -6,10 +6,11 @@ abstract class Element {
   def above(that: Element) = new ArrayElement(this.contents ++ that.contents)
 
   def beside(that: Element) = {
-    val contents = new Array[String](this.contents.length)
-    for (i <- 0 until this.contents.length)
-      contents(i) = this.contents(i) + that.contents(i)
-    new ArrayElement(contents)
+    new ArrayElement(
+      for {
+        (line1, line2) <- this.contents zip that.contents
+      } yield line1 + line2
+    )
   }
 
   final override def toString = contents.mkString("\n")
