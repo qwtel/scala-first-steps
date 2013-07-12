885aba4bf64fa5822fbca5d24c154d355d0d4e50
Florian Klampfer <f.klampfer@gmail.com>
2013-07-12 22:09:11 +0200

    implement toString

--- Element.scala
+++ Element.scala
@@ -2,6 +2,7 @@ abstract class Element {
   def contents: Array[String]
   def height = contents.length
   def width = if (height == 0) 0 else contents(0).length
+  override def toString = contents.mkString("\n")
 }
 
 class ArrayElement(val contents: Array[String]) extends Element
@@ -9,4 +10,13 @@ class ArrayElement(val contents: Array[String]) extends Element
 class LineElement(s: String) extends ArrayElement(Array(s)) {
   override def width = s.length
   override val height = 1
+}
+
+class UniformElement(
+  ch: Char,
+  override val width: Int,
+  override val height: Int
+) extends Element {
+  private val line = ch.toString * width
+  def contents = (for (i <- 1 to height) yield line).toArray
 }
