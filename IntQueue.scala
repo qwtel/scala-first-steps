29d3f004014822cb1cf2162557ea2f95ef32b7d1
Florian Klampfer <f.klampfer@gmail.com>
2013-07-13 14:26:43 +0200

    more traits

--- IntQueue.scala
+++ IntQueue.scala
@@ -15,4 +15,53 @@ trait Doubling extends IntQueue {
   abstract override def put(x: Int) = super.put(2 * x)
 }
 
+trait Incrementing extends IntQueue {
+  abstract override def put(x: Int) = super.put(x + 1)
+}
+
+trait Filtering extends IntQueue {
+  abstract override def put(x: Int) =
+    if (x >= 0) super.put(x)
+}
+
 class MyQueue extends BasicIntQueue with Doubling
+
+object IntQueue {
+  def main(args: Array[String]) = {
+    val queue = new BasicIntQueue with Doubling
+    queue.put(10)
+    println {
+      queue.get() // 20
+    }
+
+    val queue2 = new BasicIntQueue with Incrementing with Filtering
+
+    queue2.put(-1)
+    queue2.put(0)
+    queue2.put(1)
+
+    println()
+    println {
+      List(
+        queue2.get(), // 1
+        queue2.get()  // 2
+      ) mkString "\n"
+    }
+
+    val queue3 = new BasicIntQueue with Filtering with Incrementing
+
+    queue3.put(-1)
+    queue3.put(0)
+    queue3.put(1)
+
+    println()
+    println {
+      List(
+        queue3.get(), // 0
+        queue3.get(), // 1
+        queue3.get()  // 2
+      ) mkString "\n"
+    }
+  }
+}
+
