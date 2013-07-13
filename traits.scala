fc4a3305f51ea49dcc2d2b7c7220d4ded3d529b2
Florian Klampfer <f.klampfer@gmail.com>
2013-07-13 13:38:09 +0200

    DRW with traits

--- traits.scala
+++ traits.scala
@@ -1,13 +1,14 @@
 class Point(val x: Int, val y: Int)
 
-class Rectangle(val topLeft: Point, val bottomRight: Point) {
-  def left = topLeft.x
-  def right = bottomRight.x
-  def width = right - left
-  // and more..
+class Rectangle(val topLeft: Point, val bottomRight: Point) extends Rectangular {
+  // other methods...
 }
 
-abstract class Component {
+abstract class Component extends Rectangular {
+  // other methods...
+}
+
+trait Rectangular {
   def topLeft: Point
   def bottomRight: Point
 
