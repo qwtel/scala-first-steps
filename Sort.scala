559ad45e6ea217a99e1b2e161085a1d158d9e22f
Florian Klampfer <f.klampfer@gmail.com>
2013-07-16 14:49:19 +0200

    insertion sort

---
+++ Sort.scala
@@ -0,0 +1,9 @@
+object Sort {
+  def isort(xs: List[Int]): List[Int] =
+    if (xs.isEmpty) Nil
+    else insert(xs.head, isort(xs.tail))
+
+  def insert(x: Int, xs: List[Int]): List[Int] =
+    if (xs.isEmpty || x <= xs.head) x :: xs
+    else xs.head :: insert(x, xs.tail)
+}
