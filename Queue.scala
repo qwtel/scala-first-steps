222c03124864da55de00bc843666e1e34fbcdd02
Florian Klampfer <f.klampfer@gmail.com>
2013-07-18 22:18:54 +0200

    information hiding by using a trait

--- Queue.scala
+++ Queue.scala
@@ -10,31 +10,35 @@ class SlowHeadQueue[T](smele: List[T]) {
   def append(x: T) = new SlowHeadQueue(x :: smele)
 }
 
-class Queue[T] private (
-  private val leading: List[T],
-  private val trailing: List[T]
-) {
-  def this() = this(Nil, Nil)
-
-  def this(elems: T*) = this(elems.toList, Nil)
-
-  private def mirror =
-    if (leading.isEmpty)
-      new Queue(trailing.reverse, Nil)
-    else
-      this
-
-  def head = mirror.leading.head
-
-  def tail = {
-    val q = mirror
-    new Queue(q.leading.tail, q.trailing)
-  }
-
-  def append(x: T) =
-    new Queue(leading, x :: trailing)
+trait Queue[T] {
+  def head: T
+  def tail: Queue[T]
+  def append(x: T): Queue[T]
 }
 
 object Queue {
-  def apply[T](xs: T*) = new Queue[T](xs.toList, Nil)
+  def apply[T](xs: T*) = new QueueImpl[T](xs.toList, Nil)
+
+  private class QueueImpl[T] (
+    private val leading: List[T],
+    private val trailing: List[T]
+  ) extends Queue[T] {
+
+    private def mirror =
+      if (leading.isEmpty)
+        new QueueImpl(trailing.reverse, Nil)
+      else
+        this
+
+    def head = mirror.leading.head
+
+    def tail = {
+      val q = mirror
+      new QueueImpl(q.leading.tail, q.trailing)
+    }
+
+    def append(x: T) =
+      new QueueImpl(leading, x :: trailing)
+  }
+
 }
