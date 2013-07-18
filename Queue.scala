508f9d4ba3a8817888d6a1a752c61de94f60cc36
Florian Klampfer <f.klampfer@gmail.com>
2013-07-18 21:59:41 +0200

    slow head queue

--- Queue.scala
+++ Queue.scala
@@ -3,3 +3,9 @@ class SlowAppendQueue[T](elems: List[T]) {
   def tail = new SlowAppendQueue(elems.tail)
   def append(x: T) = new SlowAppendQueue(elems ::: List(x))
 }
+
+class SlowHeadQueue[T](smele: List[T]) {
+  def head = smele.last
+  def tail = new SlowHeadQueue(smele.init)
+  def append(x: T) = new SlowHeadQueue(x :: smele)
+}
