d0aebe4030b055933a42f6e85d58e19670798c75
Florian Klampfer <f.klampfer@gmail.com>
2013-07-18 22:09:17 +0200

    amortized constant time queue

--- Queue.scala
+++ Queue.scala
@@ -9,3 +9,24 @@ class SlowHeadQueue[T](smele: List[T]) {
   def tail = new SlowHeadQueue(smele.init)
   def append(x: T) = new SlowHeadQueue(x :: smele)
 }
+
+class Queue[T](
+  private val leading: List[T],
+  private val trailing: List[T]
+) {
+  private def mirror =
+    if (leading.isEmpty)
+      new Queue(trailing.reverse, Nil)
+    else
+      this
+
+  def head = mirror.leading.head
+
+  def tail = {
+    val q = mirror
+    new Queue(q.leading.tail, q.trailing)
+  }
+
+  def append(x: T) =
+    new Queue(leading, x :: trailing)
+}
