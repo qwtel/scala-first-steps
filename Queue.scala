395b348c1c727a1890c041dd88f0f9586b93acd8
Florian Klampfer <f.klampfer@gmail.com>
2013-07-19 11:31:17 +0200

    type parameter with lower bound

--- Queue.scala
+++ Queue.scala
@@ -10,16 +10,16 @@ class SlowHeadQueue[T](smele: List[T]) {
   def append(x: T) = new SlowHeadQueue(x :: smele)
 }
 
-trait Queue[T] {
+trait Queue[+T] {
   def head: T
   def tail: Queue[T]
-  def append(x: T): Queue[T]
+  def append[U >: T](x: U): Queue[U]
 }
 
 object Queue {
   def apply[T](xs: T*): Queue[T] = new QueueImpl[T](xs.toList, Nil)
 
-  private class QueueImpl[T] (
+  private class QueueImpl[+T] (
     private val leading: List[T],
     private val trailing: List[T]
   ) extends Queue[T] {
@@ -37,7 +37,7 @@ object Queue {
       new QueueImpl(q.leading.tail, q.trailing)
     }
 
-    def append(x: T) =
-      new QueueImpl(leading, x :: trailing)
+    def append[U >: T](x: U) =
+      new QueueImpl[U](leading, x :: trailing)
   }
 }
