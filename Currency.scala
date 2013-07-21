cf8d8de11a794c10a20d78b1262653686d798a0b
Florian Klampfer <f.klampfer@gmail.com>
2013-07-21 21:33:26 +0200

    add other currencies

--- Currency.scala
+++ Currency.scala
@@ -2,21 +2,25 @@ abstract class CurrencyZone {
   type Currency <: AbstractCurrency
   def make(amount: Long): Currency // factory method
   abstract class AbstractCurrency {
+
     val amount: Long
     def designation: String
-    override def toString = amount + " " + designation
+
     def +(that: Currency): Currency = make(this.amount + that.amount)
     def *(x: Double): Currency = make((this.amount * x).toLong)
+
+    private def decimals(n: Long): Int = if (n == 1) 0 else 1 + decimals(n / 10)
+    override def toString = (amount.toDouble / CurrencyUnit.amount.toDouble)
+      .formatted("%."+ decimals(CurrencyUnit.amount) +"f") +
+      " "+ designation
   }
+
+  val CurrencyUnit: Currency
 }
 
 object US extends CurrencyZone {
   abstract class Dollar extends AbstractCurrency {
     def designation = "USD"
-    override def toString = (amount.toDouble / CurrencyUnit.amount.toDouble)
-      .formatted("%."+ decimals(CurrencyUnit.amount) +"f") +
-      " "+ designation
-    private def decimals(n: Long): Int = if (n == 1) 0 else 1 + decimals(n / 10)
   }
   type Currency = Dollar
   def make(cents: Long) = new Dollar { val amount = cents }
@@ -24,4 +28,27 @@ object US extends CurrencyZone {
   val Cent = make(1)
   val Dollar = make(100)
   val CurrencyUnit = Dollar
+}
+
+object Europe extends CurrencyZone {
+  abstract class Euro extends AbstractCurrency {
+    def designation = "EUR"
+  }
+  type Currency = Euro
+  def make(cents: Long) = new Euro { val amount = cents }
+
+  val Cent = make(1)
+  val Euro = make(100)
+  val CurrencyUnit = Euro
+}
+
+object Japan extends CurrencyZone {
+  abstract class Yen extends AbstractCurrency {
+    def designation = "JPY"
+  }
+  type Currency = Yen
+  def make(yen: Long) = new Yen { val amount = yen }
+
+  val Yen = make(1)
+  val CurrencyUnit = Yen
 }
