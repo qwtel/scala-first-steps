6d53038d870e7bdcf18c56531b30ba15d8c8aae0
Florian Klampfer <f.klampfer@gmail.com>
2013-07-21 21:26:54 +0200

    add support for cents

--- Currency.scala
+++ Currency.scala
@@ -13,7 +13,15 @@ abstract class CurrencyZone {
 object US extends CurrencyZone {
   abstract class Dollar extends AbstractCurrency {
     def designation = "USD"
+    override def toString = (amount.toDouble / CurrencyUnit.amount.toDouble)
+      .formatted("%."+ decimals(CurrencyUnit.amount) +"f") +
+      " "+ designation
+    private def decimals(n: Long): Int = if (n == 1) 0 else 1 + decimals(n / 10)
   }
   type Currency = Dollar
-  def make(x: Long) = new Dollar { val amount = x }
+  def make(cents: Long) = new Dollar { val amount = cents }
+
+  val Cent = make(1)
+  val Dollar = make(100)
+  val CurrencyUnit = Dollar
 }
