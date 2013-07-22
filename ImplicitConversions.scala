6bf7240c98b3a97240cc9d98f97651ad5049f43c
Florian Klampfer <f.klampfer@gmail.com>
2013-07-22 10:50:09 +0200

    add converstion method

--- ImplicitConversions.scala
+++ ImplicitConversions.scala
@@ -1,11 +1,14 @@
 import java.awt.event.{ActionEvent, ActionListener}
 import javax.swing.JButton
 
+implicit def function2ActionListener(f: ActionEvent => Unit) =
+  new ActionListener {
+    def actionPerformed(e: ActionEvent) = f(e)
+  }
+
 val button = new JButton
 button.addActionListener(
-  new ActionListener {
-    def actionPerformed(e: ActionEvent) {
-      println("pressed!")
-    }
-  }
+  function2ActionListener(
+    (_: ActionEvent) => println("pressed!")
+  )
 )
