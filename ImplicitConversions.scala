5a6b602dbc5e5c80917c39edb25a991eeb1c6fb8
Florian Klampfer <f.klampfer@gmail.com>
2013-07-22 11:02:53 +0200

    my boolean

--- ImplicitConversions.scala
+++ ImplicitConversions.scala
@@ -10,3 +10,12 @@ val button = new JButton
 button.addActionListener(
   (_: ActionEvent) => println("pressed!")
 )
+
+implicit def boolean2MyBoolean(b: Boolean) = new MyBoolean(b)
+
+class MyBoolean(b: Boolean) {
+  def and(o: Boolean) = o && b
+  def or(o: Boolean) = o || b
+  def is(o: Boolean) = o == b
+  def isnt(o: Boolean) = o != b
+}
