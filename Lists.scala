31e43b61f97c6fc8dc8ca0ed59314ada78595d61
Florian Klampfer <f.klampfer@gmail.com>
2013-07-23 18:15:23 +0200

    performance test

--- Lists.scala
+++ Lists.scala
@@ -12,20 +12,20 @@ package my {
       else tail.drop(n - 1)
 
     def map[U](f: T => U): List[U] = {
-      def mapHelper(res: List[U]): List[U] =
-        this match {
+      def mapHelper(list: List[T], res: List[U]): List[U] =
+        list match {
+          case x :: xs => mapHelper(xs, f(x) :: res)
           case Nil => res
-          case x :: xs => mapHelper(f(x) :: res)
         }
-      mapHelper(Nil).reverse
+      mapHelper(this, Nil).reverse
     }
 
     def reverse: List[T] = this.foldLeft(List[T]())((ys, y) => y :: ys)
 
     private def foldLeft[B](zero: B)(f: (B, T) => B): B =
       this match {
-        case Nil => zero
         case x :: xs => xs.foldLeft(f(zero, x))(f)
+        case Nil => zero
       }
 
     def ::[U >: T](x: U): List[U] = new ::(x, this)
@@ -50,5 +50,37 @@ package my {
 
   object List {
     def apply[T](): List[T] = Nil
+  }
+
+  object PerformanceTest extends App {
+    def time[A](f: => A) = {
+      val s = System.nanoTime
+      val ret = f
+      println("time: "+(System.nanoTime-s)/1e9+"s")
+      ret
+    }
+
+    //val x = 1 :: 2 :: 3 :: 4 :: 5 :: 6 :: 7 :: 8 :: 9 :: 10 :: Nil
+    //val y = 1 :: 2 :: 3 :: 4 :: 5 :: 6 :: 7 :: 8 :: 9 :: 10 :: scala.collection.immutable.Nil
+
+    var x = List[Int]()
+    (1 to 1000000).foreach((i) => x = i :: x)
+    time {
+      x.map(_ + 1)
+      x.map(_ * 2)
+      x.map(_ / 2)
+      x.map(_ - 1)
+      x.map(_.toString)
+    }
+
+    var y = scala.collection.immutable.List[Int]()
+    (1 to 1000000).foreach((i) => y = i :: y)
+    time {
+      y.map(_ + 1)
+      y.map(_ * 2)
+      y.map(_ / 2)
+      y.map(_ - 1)
+      y.map(_.toString)
+    }
   }
 }
