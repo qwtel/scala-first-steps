625a0128c793506abde7c9339febb9b80315bee7
Florian Klampfer <f.klampfer@gmail.com>
2013-07-23 13:59:11 +0200

    wrap into package, introduce list methods

--- Lists.scala
+++ Lists.scala
@@ -1,18 +1,28 @@
-sealed abstract class MyList[+T] {
-  def isEmpty: Boolean
-  def head: T
-  def tail: MyList[T]
+package my {
+  sealed abstract class List[+T] {
+    def isEmpty: Boolean
+    def head: T
+    def tail: List[T]
+
+    def length: Int =  if (isEmpty) 0  else 1 + tail.length
+
+    def drop(n: Int): List[T] =
+      if (isEmpty) Nil
+      else if (n <= 0) this
+      else tail.drop(n - 1)
+  }
+
+  case object Nil extends List[Nothing] {
+    def isEmpty: Boolean = true
+    def head: Nothing = throw new NoSuchElementException("head of empty list")
+    def tail: List[Nothing] = throw new NoSuchElementException("tail of empty list")
+  }
+
+  // pronounced "cons" for "construct"
+  // enables pattern matching like x :: xs
+  // which is the same as ::(x, xs)
+  final case class ::[T](head: T, tail: List[T]) extends List[T] {
+    def isEmpty: Boolean = false
+  }
 }
 
-case object Nil extends MyList[Nothing] {
-  def isEmpty: Boolean = true
-  def head: Nothing = throw new NoSuchElementException("head of empty list")
-  def tail: MyList[Nothing] = throw new NoSuchElementException("tail of empty list")
-}
-
-// pronounced "cons" for "construct"
-// enables pattern matching like x :: xs
-// which is the same as ::(x, xs)
-final case class ::[T](head: T, tail: MyList[T]) extends MyList[T] {
-  def isEmpty: Boolean = false
-}
