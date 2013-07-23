dd016051b10ce6eecac782937efdeddee11bcb0e
Florian Klampfer <f.klampfer@gmail.com>
2013-07-23 13:42:12 +0200

    my own list implementation

---
+++ Lists.scala
@@ -0,0 +1,20 @@
+sealed abstract class MyList[+T] {
+  def isEmpty: Boolean
+  def head: T
+  def tail: MyList[T]
+}
+
+case object Nil extends MyList[Nothing] {
+  def isEmpty: Boolean = true
+  def head: Nothing = throw new NoSuchElementException("head of empty list")
+  def tail: MyList[Nothing] = throw new NoSuchElementException("tail of empty list")
+}
+
+// pronounced "cons" for "construct"
+// enables pattern matching like x :: xs
+// which is the same as ::(x, xs)
+final case class ::[T](hd: T, tl: MyList[T]) extends MyList[T] {
+  def isEmpty: Boolean = false
+  def head: T = hd
+  def tail: MyList[T] = tl
+}
