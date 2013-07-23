56aaecd177c424ea500b4910ce2f18a30995d390
Florian Klampfer <f.klampfer@gmail.com>
2013-07-23 14:46:06 +0200

    scala weirdness

--- Lists.scala
+++ Lists.scala
@@ -10,6 +10,18 @@ package my {
       if (isEmpty) Nil
       else if (n <= 0) this
       else tail.drop(n - 1)
+
+    def map[U](f: T => U): List[U] =
+      if (isEmpty) Nil
+      else f(head) :: tail.map(f)
+
+    // these to methods are necessary to make the x :: xs syntax possible
+    // Anything here is implicitly converted to a list
+    // then it has a ::-method that returns a new list construct
+    // methods that end with : are applied in reverse order, e.g. x :: xs == xs.::(x)
+    // the type parameter [S >: T] is to support covariant lists, e.g. List[Cat] <: List[Animal]
+    implicit def asList[T](a: T): List[T] = new ::(a, Nil)
+    def ::[S >: T](x: S): List[S] = new ::(x, this)
   }
 
   case object Nil extends List[Nothing] {
@@ -21,8 +33,19 @@ package my {
   // pronounced "cons" for "construct"
   // enables pattern matching like x :: xs
   // which is the same as ::(x, xs)
-  final case class ::[T](head: T, tail: List[T]) extends List[T] {
+  final case class ::[T](head: T, tail: List[T] = Nil) extends List[T] {
     def isEmpty: Boolean = false
   }
 }
 
+/*
+::(1)
+::(1, Nil)
+::(1, 2)
+::(1, asList(2))
+::(1, ::(2))
+3.::(asList(1), 2)
+2.::(asList(1))
+
+1 :: 2 // a lot of weirdness going on to make this possible
+*/
