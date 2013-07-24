9354ac351732f1090bd2d700cc16e2fdcec757c4
Florian Klampfer <f.klampfer@gmail.com>
2013-07-24 11:30:27 +0200

    first actors

---
+++ Actors.scala
@@ -0,0 +1,29 @@
+import scala.actors._
+
+object SillyActor extends Actor {
+  def act() {
+    for (i <- 1 to 5) {
+      println("I'm acting!")
+      Thread.sleep(1000)
+    }
+  }
+}
+
+object SeriousActor extends Actor {
+  def act() {
+    for (i <- 1 to 5) {
+      println("To be or not to be.")
+      Thread.sleep(1000)
+    }
+  }
+}
+
+object EchoActor extends Actor {
+  def act() {
+    while (true) {
+      receive {
+        case msg => println("received message: " + msg)
+      }
+    }
+  }
+}
