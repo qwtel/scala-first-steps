729768af32db5a13b322a067daf9de44d55cf9da
Florian Klampfer <f.klampfer@gmail.com>
2013-07-24 13:20:22 +0200

    implementation of the DSL

--- Wire.scala
+++ Wire.scala
@@ -35,16 +35,74 @@ package simulation {
       }
     }
   }
+
+  abstract class BasicCircuitSimulation extends Simulation {
+    val InverterDelay: Int
+    val AndGateDelay: Int
+    val OrGateDelay: Int
+
+    class Wire {
+      private var sigVal = false
+      private var actions: List[Action] = List()
+
+      def getSignal = sigVal
+
+      def setSignal(s: Boolean) =
+        if (s != sigVal) {
+          sigVal = s
+          actions foreach (action => action())
+        }
+
+      def addAction(action: Action) = {
+        actions = action :: actions
+        action()
+      }
+    }
+
+    def inverter(input: Wire, output: Wire) = {
+      def invertAction() {
+        val inputSig = input.getSignal
+        afterDelay(InverterDelay) {
+          output setSignal !inputSig
+        }
+      }
+      input addAction invertAction
+    }
+
+    def andGate(a1: Wire, a2: Wire, output: Wire) = {
+      def andAction() = {
+        val a1Sig = a1.getSignal
+        val a2Sig = a2.getSignal
+        afterDelay(AndGateDelay) {
+          output setSignal (a1Sig & a2Sig)
+        }
+      }
+      a1 addAction andAction
+      a2 addAction andAction
+    }
+
+    def orGate(o1: Wire, o2: Wire, output: Wire) = {
+      def orAction() = {
+        val o1Sig = o1.getSignal
+        val o2Sig = o2.getSignal
+        afterDelay(AndGateDelay) {
+          output setSignal (o1Sig | o2Sig)
+        }
+      }
+      o1 addAction orAction
+      o2 addAction orAction
+    }
+
+    def probe(name: String, wire: Wire) {
+      def probeAction() {
+        println(name + " " + currentTime + " nav-value = " + wire.getSignal)
+      }
+      wire addAction probeAction
+    }
+  }
 }
 
 /*
-class Wire
-val a, b, c = new Wire
-
-def inverter(input: Wire, output: Wire)
-def andGate(a1: Wire, a2: Wire, output: Wire)
-def orGate(o1: Wire, o2: Wire, output: Wire)
-
 // produces s = (a + b) % 2 and a carry c = (a + b) / 2
 def halfAdder(a: Wire, b: Wire, s: Wire, c: Wire) {
   val d, e = new Wire
