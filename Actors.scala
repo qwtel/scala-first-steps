6022d0e9df26511ceabf3a9a6e923257bb912fb8
Florian Klampfer <f.klampfer@gmail.com>
2013-07-24 11:40:33 +0200

    using react instead of act

--- Actors.scala
+++ Actors.scala
@@ -1,3 +1,4 @@
+import java.net.UnknownHostException
 import scala.actors._
 
 object SillyActor extends Actor {
@@ -24,6 +25,32 @@ object EchoActor extends Actor {
       receive {
         case msg => println("received message: " + msg)
       }
+    }
+  }
+}
+
+object NameResolver extends Actor {
+  import java.net.{InetAddress, UnknownHostException}
+
+  def act() {
+    react {
+      case (name: String, actor: Actor) =>
+        actor ! getIp(name)
+        act()
+      case "EXIT" =>
+        println("Name resolver exiting.")
+        // quit
+      case msg =>
+        println("Unhandled message: "+ msg)
+        act()
+    }
+  }
+
+  def getIp(name: String): Option[InetAddress] = {
+    try {
+      Some(InetAddress.getByName(name))
+    } catch {
+      case _: UnknownHostException => None
     }
   }
 }
