6c3cf7e58aefa5dd7c78a526b8120e9c7ef145ba
Florian Klampfer <f.klampfer@gmail.com>
2013-07-24 12:56:48 +0200

    add simulation framework

--- Wire.scala
+++ Wire.scala
@@ -1,3 +1,43 @@
+package simulation {
+  class Simulation {
+    type Action = () => Unit
+
+    case class WorkItem(time: Int, action: Action)
+
+    private var curtime = 0
+    def currentTime = curtime
+
+    private var agenda: List[WorkItem] = List()
+
+    private def insert(ag: List[WorkItem], item: WorkItem): List[WorkItem] = {
+      if (ag.isEmpty || item.time < ag.head.time) item :: ag
+      else ag.head :: insert(ag.tail, item)
+    }
+
+    def afterDelay(delay: Int)(block: => Unit) {
+      val item = WorkItem(currentTime + delay, () => block)
+      agenda = insert(agenda, item)
+    }
+
+    def run() {
+      afterDelay(0) {
+        println("*** simulation started, time = " + currentTime + " ***")
+      }
+      while(!agenda.isEmpty) next()
+    }
+
+    def next() {
+      (agenda: @unchecked) match {
+        case item :: rest =>
+          agenda = rest
+          curtime =item.time
+          item.action()
+      }
+    }
+  }
+}
+
+/*
 class Wire
 val a, b, c = new Wire
 
@@ -20,3 +60,4 @@ def fullAdder(a: Wire, b: Wire, cin: Wire, sum: Wire, cout: Wire) {
   halfAdder(b, s, sum, c2)
   orGate(c1, c2, cout)
 }
+*/
